3f5c2c4a9b490f474cc69f7dc9a0013874dcaadd
Quokka <wotjr1142@gmail.com>
2024-04-11 18:06:58 +0900

    first commit

---
+++ chap01-literal-and-variable/src/main/java/com/ohgiaffers/section/literal/Application01.java
@@ -0,0 +1,33 @@
+package main.java.com.ohgiaffers.section.literal;
+
+public class Application01 {
+
+    public  static  void main(String[] args) {
+        // 리터럴이란 값 자체를 의미한다.
+
+        // 정수형 리터럴
+        System.out.println(123);
+
+        // 실수형 리터럴
+        System.out.println(1.23);
+
+        // 문자형 리터럴
+        System.out.println('A');
+        // 문자열형 리터럴
+        System.out.println("Hello world");
+
+        System.out.println("12345");
+        System.out.println("123.45");
+
+        // 논리형 리터럴
+        System.out.println(true);
+        System.out.println(false);
+
+
+    }
+}
+
+
+
+
+
---
+++ chap01-literal-and-variable/src/main/java/com/ohgiaffers/section/literal/Application02.java
@@ -0,0 +1,22 @@
+package main.java.com.ohgiaffers.section.literal;
+
+public class Application02 {
+    public  static  void main(String[] args){
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2/2);
+        System.out.println(2*2);
+
+        System.out.println(123%10);
+
+
+        System.out.println(1.23+1.23);
+        System.out.println(1.23-1.23);
+        System.out.println(1.23*1.23);
+        System.out.println(1.23/1.23);
+        System.out.println(1.23%1.0);
+
+
+    }
+}
+
